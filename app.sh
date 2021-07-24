83494e9b9196b9e8b5e94d652ce4007361af25c2
John Steel <key@jskw.dev>
2021-07-24 12:34:48 -0400

    init

---
+++ app.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source lib/colour.sh
+colour
---
+++ lib/colour.sh
@@ -0,0 +1,18 @@
+hello() {
+  echo "Hello ${1}!"
+}
+
+colour() {
+  #https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+  for i in $(seq 0 15)
+  do
+    for j in $(seq 0 15)
+    do
+        code=$((i * 16 + j))
+        printf "\u001b[38;5;${code}m%4s" "$code"
+    done
+    echo -n "\n"
+  done
+
+  printf "\u001b[0m"
+}
