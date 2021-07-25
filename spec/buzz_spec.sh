66dd14586e3614017d045768dc8f682d955b3a5c
John Steel <key@jskw.dev>
2021-07-25 14:53:03 -0400

    More buzz tests

--- spec/buzz_spec.sh
+++ spec/buzz_spec.sh
@@ -2,18 +2,22 @@ Describe 'fizzbuzz'
   Include lib/fizz_buzz.sh
 
   Parameters
-    1 1
-    2 2
-    3 "BUZZ!"
-    4 4
-    5 "FIZZ!"
-    6 6
-    13 "BUZZ!"
-    15 "FIZZ!"
-    35 "FIZZBUZZ!"
-    53 "FIZZBUZZ!"
+    1   "1"
+    2   "2"
+    3   "BUZZ!"
+    4   "4"
+    5   "FIZZ!"
+    6   "6"
+    13  "BUZZ!"
+    15  "FIZZ!"
+    35  "FIZZBUZZ!"
+    53  "FIZZBUZZ!"
     350 "FIZZBUZZ!"
     305 "FIZZBUZZ!"
+    503 "FIZZBUZZ!"
+    504 "FIZZ!"
+    505 "FIZZ!"
+    606 "606"
   End
 
   It "should say $2 for $1"
